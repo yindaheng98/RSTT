b014df08bf5f8c5e5aefb72ecaab8e9a25ce9075
yindaheng98 <yindaheng98@gmail.com>
2022-07-01 17:32:45 +0800

    bug fixed

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm -rf venv
 python3 -m venv venv
 source ./venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu116

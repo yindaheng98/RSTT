3b63fc09db1ec488a3aa8335d1f25030446ed5cb
yindaheng98 <yindaheng98@gmail.com>
2022-07-03 11:20:52 +0800

    fix it

--- datasets.sh
+++ datasets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 curl -o vimeo90k/vimeo_septuplet.zip http://data.csail.mit.edu/tofu/dataset/vimeo_septuplet.zip
-cp datasets/vimeo_septuplet/*.txt vimeo90k/
-python ./datasets/prepare_vimeo.py --path vimeo90k/
+unzip -n ~/vimeo_septuplet.zip -d ~/RSTT/vimeo90k/
+cp datasets/vimeo_septuplet/*.txt vimeo90k/vimeo_septuplet
+python ./datasets/prepare_vimeo.py --path vimeo90k/vimeo_septuplet

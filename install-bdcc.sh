24594e14af41575a4c360926055947f683f73201
yindaheng98 <yindaheng98@gmail.com>
2022-07-05 10:43:55 +0800

    install in bdcc

---
+++ install-bdcc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /seu_share/home/dongfang/df_yindh/RSTT
+module load anaconda3
+module load cuda-11.6
+conda init --all
+conda create --yes --name venv
+conda activate venv
+conda install --yes --file requirements.txt -c pytorch -c conda-forge

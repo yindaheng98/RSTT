0527b3a69d5e239d8297e19dcc34ae9ac40bba28
yindaheng98 <yindaheng98@gmail.com>
2022-07-06 23:07:08 +0800

    bug fix

---
+++ count.py
@@ -0,0 +1,8 @@
+import glob
+import re
+for path in glob.glob('experiments/RSTT-S_archived_220705-001733/models/*.log'):
+    with open(path, "r") as f:
+        print(path)
+        s = f.read()
+        m = re.search(r"Total Average PSNR: ([0-9.]+?) dB", s)
+        print(m.group(1))
--- eval_seq_vimeo90k.py
+++ eval_seq_vimeo90k.py
@@ -4,6 +4,6 @@ from eval_vimeo90k import read_config, main, parser
 parser.add_argument('--pretrain_model', type=str)
 config = read_config()
 args = parser.parse_args()
-config["pretrain_model"] = args.pretrain_model
-print(config["pretrain_model"])
+config['path']['pretrain_model'] = args.pretrain_model
+print(config['path']['pretrain_model'])
 main(config)
